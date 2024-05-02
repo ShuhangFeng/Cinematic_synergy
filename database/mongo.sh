a0d77c9cf42384dbd821e49188140b27a55e815e
shuhangfeng <f625227857@gmail.com>
2024-05-02 16:56:48 -0700

    overwrite mongo.sh

--- database/mongo.sh
+++ database/mongo.sh
@@ -1,6 +1,6 @@
-# sudo mkdir -p /var/lib/mongodb1 /var/lib/mongodb2 /var/lib/mongodb3
-# sudo mkdir -p /var/log/mongodb
-# sudo chown -R $(whoami) /var/lib/mongodb1 /var/lib/mongodb2 /var/lib/mongodb3 /var/log/mongodb
+sudo mkdir -p /var/lib/mongodb1 /var/lib/mongodb2 /var/lib/mongodb3
+sudo mkdir -p /var/log/mongodb
+sudo chown -R $(whoami) /var/lib/mongodb1 /var/lib/mongodb2 /var/lib/mongodb3 /var/log/mongodb
 
 mongod --config mongod1.conf
 mongod --config mongod2.conf
